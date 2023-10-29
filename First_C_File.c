debd2cba624dfa9c25f1ef6fe6a59f07637751ba
Ashik0408 <ashikt128@gmail.com>
2023-10-29 09:29:35 +0530

    New file added

--- First_C_File.c
+++ First_C_File.c
@@ -3,11 +3,11 @@ int main()
 {
 
     printf("Karan Mohod");
-    printf("hello XXX");
     printf("hello everyone");
     printf("hello Ashik");
     printf("hello samiksha");
     printf("hello Pranav");
+    
     return 0;
 }
 
